ae6258b4ce6f89c9552bc646ed737b8360126d5a
Priyanshu Sidar <sidarsunita2@gmail.com>
2024-08-29 02:36:00 +0530

    added input phrase page

--- lib/pages/inputPhrase.dart
+++ lib/pages/inputPhrase.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nft_gallary_app/utils/buttonX.dart';
+import 'package:bip39/bip39.dart' as bip39;
 
 class InputPhrasePage extends StatefulWidget {
   const InputPhrasePage({super.key});
@@ -8,8 +11,71 @@ class InputPhrasePage extends StatefulWidget {
 }
 
 class _InputPhrasePageState extends State<InputPhrasePage> {
+  final _formKey = GlobalKey<FormState>();
+  final _words = List<String>.filled(12, '');
+  bool validationFailed = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text('Please enter your recovery phrase',
+              style: TextStyle(fontSize: 18)),
+          const SizedBox(height: 32),
+          Center(
+            child: Form(
+              key: _formKey,
+              child: SizedBox(
+                  width: 300,
+                  height: 400,
+                  child: GridView.count(
+                    padding: const EdgeInsets.all(3),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 3,
+                    shrinkWrap: true,
+                    crossAxisCount: 3,
+                    children: List.generate(12, (index) {
+                      return SizedBox(
+                        height: 50,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            hintText: '${index + 1}',
+                          ),
+                          onSaved: (value) {
+                            _words[index] = value!;
+                          },
+                        ),
+                      );
+                    }),
+                  )),
+            ),
+          ),
+          Text(validationFailed ? 'Invalid keyphrase' : '',
+              style: const TextStyle(color: Colors.red)),
+          const Spacer(),
+          ButtonX(buttonLable: "Continue", click: () => _onSubmit)
+        ],
+      ),
+    );
+  }
+
+  void _onSubmit(context) async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      String wordsString = _words.join(' ');
+      final t = bip39.validateMnemonic(wordsString);
+      if (t) {
+        GoRouter.of(context).go("/passwordSetup/$wordsString");
+      } else {
+        setState(() {
+          validationFailed = true;
+        });
+      }
+    }
   }
 }
